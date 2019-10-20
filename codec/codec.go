e248cb383fa1c042601cdcad8c4e32c544d6554a
JayB-K <jaybkim2@gmail.com>
2019-10-21 00:08:37 +0900

    Add codec: apply Cosmos SDK v0.34.9

---
+++ codec/codec.go
@@ -0,0 +1,14 @@
+package codec
+
+import (
+	"github.com/cosmos/cosmos-sdk/cmd/gaia/app"
+	"github.com/cosmos/cosmos-sdk/codec"
+)
+
+// Codec is the application-wide Amino codec and is initialized upon package loading.
+var Codec *codec.Codec
+
+func init() {
+	Codec = app.MakeCodec()
+	Codec.Seal()
+}
